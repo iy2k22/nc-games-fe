1698d53dabc9f8e427bc05cb9ce21bd9c5acbae2
Ibrahim Yusuf <iy830755@gmail.com>
2023-11-08 16:05:17 +0000

    add bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,20 @@ import { Link } from "react-router-dom";
 
 const Header = () => (
     <header>
-        <h1>NC Games</h1>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/reviews">Reviews</Link>
-                </li>
-                <li>
-                    <Link to="/categories">Categories</Link>
-                </li>
-            </ul>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="#">NC Games</Link>
+                <div id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/reviews" aria-current="page">Reviews</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/categories">Categories</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     </header>
 );
--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const ReviewCard = ({ id, owner, title, img, votes, created_at }) => (
-	<div className="reviewCard">
+/* 	<div className="reviewCard">
 		<Link to={`/reviews/${id}`}>
 			<h2>{title}</h2>
 		</Link>
@@ -9,6 +9,15 @@ const ReviewCard = ({ id, owner, title, img, votes, created_at }) => (
 		<img src={img} alt={title} />
 		<h4>votes: {votes}</h4>
 		<h4>created at: {created_at}</h4>
+	</div> */
+	<div className="card">
+		<img src={img} className="card-img-top" alt={title} />
+		<div className="card-body">
+			<h5 className="card-title">{title}</h5>
+			<p className="card-text"><i className="fa fa-thumbs-up"></i> {votes}</p>
+			<p className="card-text"><i className="fa fa-clock-o"></i> {created_at}</p>
+			<Link to={`/reviews/${id}`} className="btn btn-primary">View</Link>
+		</div>
 	</div>
 )
 
